a1e88bcd79588d087965a7f1106dc4358187df99
Jesse Farinacci <jieryn@gmail.com>
2011-02-02 13:44:06 -0500

    first commit... also a fully functioning plugin

---
+++ src/main/java/hudson/plugins/column/console/LastSuccessfulBuildColumn.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2011, Jesse Farinacci
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.column.console;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.plugins.column.Messages;
+import hudson.views.ListViewColumn;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * @author <a href="mailto:jieryn@gmail.com">Jesse Farinacci</a>
+ * @since 1.0
+ */
+public final class LastSuccessfulBuildColumn extends ListViewColumn
+{
+  private static final class ConsoleColumnDescriptor extends
+      Descriptor<ListViewColumn>
+  {
+    @Override
+    public String getDisplayName()
+    {
+      return Messages.ConsoleColumn_Last_Successful_DisplayName();
+    }
+
+    @Override
+    public ListViewColumn newInstance(final StaplerRequest request,
+        final JSONObject formData) throws FormException
+    {
+      return new LastSuccessfulBuildColumn();
+    }
+  }
+
+  @Extension
+  public static final Descriptor<ListViewColumn> DESCRIPTOR = new ConsoleColumnDescriptor();
+
+  @Override
+  public Descriptor<ListViewColumn> getDescriptor()
+  {
+    return DESCRIPTOR;
+  }
+}
