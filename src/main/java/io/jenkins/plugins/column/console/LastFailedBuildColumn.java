718572330e535a40a36a8225360b3044586e17ed
NotMyFault <mc.cache@web.de>
2022-02-14 19:44:00 +0100

    More refactoring

--- src/main/java/io/jenkins/plugins/column/console/LastFailedBuildColumn.java
+++ src/main/java/io/jenkins/plugins/column/console/LastFailedBuildColumn.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2011, Jesse Farinacci
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,17 +30,28 @@ import hudson.views.ListViewColumnDescriptor;
 import hudson.views.ListViewColumn;
 import net.sf.json.JSONObject;
 
-import org.jenkins.ci.plugins.column.Messages;
+import io.jenkins.plugins.column.Messages;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
  * A {@link ListViewColumn} which adds a fast-path icon to the
  * <code>lastFailedBuild</code> console output.
- * 
+ *
  * @author <a href="mailto:jieryn@gmail.com">Jesse Farinacci</a>
  * @since 1.0
  */
 public final class LastFailedBuildColumn extends ListViewColumn {
+    /**
+     * The plugin descriptor.
+     */
+    @Extension
+    public static final Descriptor<ListViewColumn> DESCRIPTOR = new ConsoleColumnDescriptor();
+
+    @Override
+    public Descriptor<ListViewColumn> getDescriptor() {
+        return DESCRIPTOR;
+    }
+
     /**
      * The plugin descriptor.
      */
@@ -53,7 +64,7 @@ public final class LastFailedBuildColumn extends ListViewColumn {
 
         @Override
         public ListViewColumn newInstance(final StaplerRequest request,
-                final JSONObject formData) throws FormException {
+                                          final JSONObject formData) {
             return new LastFailedBuildColumn();
         }
 
@@ -61,16 +72,5 @@ public final class LastFailedBuildColumn extends ListViewColumn {
         public boolean shownByDefault() {
             return false;
         }
-    }
-
-    /**
-     * The plugin descriptor.
-     */
-    @Extension
-    public static final Descriptor<ListViewColumn> DESCRIPTOR = new ConsoleColumnDescriptor();
-
-    @Override
-    public Descriptor<ListViewColumn> getDescriptor() {
-        return DESCRIPTOR;
     }
 }
